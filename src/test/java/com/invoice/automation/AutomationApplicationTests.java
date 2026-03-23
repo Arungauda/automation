a3099a154e271e36b7fae582087a7844b286a69f
Arun Gauda <arun.gauda@nagarro.com>
2026-03-23 14:38:50 +0530

    Initial commit

---
+++ src/test/java/com/invoice/automation/AutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invoice.automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
